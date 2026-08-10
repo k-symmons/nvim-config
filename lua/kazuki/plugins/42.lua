364e819cb8c48885620ff705025200e934096b9e
ksymmons <kazukisymmons+dev@gmail.com>
2026-08-10 20:16:03 +0900

    42 linter changes

--- lua/kazuki/plugins/42.lua
+++ lua/kazuki/plugins/42.lua
@@ -1,14 +1,27 @@
 return {
-  "Diogo-ss/42-header.nvim",
-  cmd = { "Stdheader" },
-  keys = { "<F1>" },
-  opts = {
-    default_map = true,
-    auto_update = true,
-	user = "ksymmons",
-	mail = "ksymmons@student.42tokyo.jp",
-  },
-  config = function(_, opts)
-    require("42header").setup(opts)
-  end,
+	{
+	  "Diogo-ss/42-header.nvim",
+	  cmd = { "Stdheader" },
+	  keys = { "<F1>" },
+	  opts = {
+		default_map = true,
+		auto_update = true,
+		user = "ksymmons",
+		mail = "ksymmons@student.42tokyo.jp",
+	  },
+	  config = function(_, opts)
+		require("42header").setup(opts)
+	  end,
+	},
+	{
+		"hardyrafael17/norminette42.nvim",
+		config = function()
+		local norminette = require("norminette")
+		norminette.setup({
+				runOnSave = true,
+				maxErrorsToShow = 5,
+				active = true,
+		})
+	end,
+	}
 }
